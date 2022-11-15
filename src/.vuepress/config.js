419026e238711acaffe44f92352104759feb6eab
Apawn1979 <apawn@foxmail.com>
2022-11-15 16:10:59 +0800

    chore: 修改目录结构

---
+++ src/.vuepress/config.js
@@ -0,0 +1,28 @@
+const nav = require('./nav-bar')
+const sidebar = require('./side-bar')
+module.exports = {
+  port: 7910,
+  dest: './docs',
+  plugins: [
+    'mermaidjs',
+    'flowchart'
+  ],
+  markdown: {
+    lineNumbers: true,
+    plugins: [
+      'katex'
+    ]
+  },
+  head: [
+    ['link', { rel: 'stylesheet', href: '/katex-css/katex.min.css' }],
+    ['link', { rel: "stylesheet", href: "/katex-css/github-markdown.min.css" }]
+  ],
+  title: '满分笔记',
+  description: '记录工作和学习中有用的信息',
+  themeConfig: {
+    logo: '/icon-100.png',
+    nav,
+    sidebar,
+    sidebarDepth: 0
+  }
+}
